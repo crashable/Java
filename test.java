294de2a2c52fdcaf50f81a02815f8107832509ac
bdnguye <bdnguye@student.matnat.uio.no>
2019-02-06 00:07:52 +0100

    Updates.

--- test.java
+++ test.java
@@ -1,7 +1,11 @@
-public class Test {
-    public static void main(String[] args) {
-        // Prints "Test" to the terminal window.
-        System.out.println("Test");
+public class test {
+  boolean b = false;
+  int a = 0;
+  public void test() {
+    if (a&&b == 0) {
+      System.out.println("Test clear.");
+    } else {
+      System.out.println("Test failed.");
     }
-
+  }
 }
