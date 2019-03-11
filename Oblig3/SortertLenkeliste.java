6478794edba88b9e10781f4f8c5490b6f5c4e01a
Nghi Ng <Nghi_96@hotmail.com>
2019-03-11 22:11:54 +0100

    done! !

--- Oblig3/SortertLenkeliste.java
+++ Oblig3/SortertLenkeliste.java
@@ -1,24 +1,54 @@
-public class SortertLenkeliste<T extrends Comparable<T>> extends Lenkelist<T> {
+public class SortertLenkeliste<T extends Comparable<T>> extends Lenkeliste<T> {
   public void leggTil(T x) {
     Node p = start;
     Node tmp = new Node(x);
     Node current;
+    if (size == 0) {
+      p = new Node(x);
+      start = p;
+      size++;
+      return;
+    }
     if (tmp.data.compareTo(p.data) < 0) {
       tmp.neste = p;
       start = tmp;
+      size++;
+      return;
     } else {
-      current = p;
+      current = p;  //Current is the node before p.
       p = p.neste;
+      if (current.neste == null) {
+        current.neste = tmp;
+        size++;
+        return;
+      }
       for (int i=0; i<size; i++) {
         if (tmp.data.compareTo(p.data) < 0) {
           tmp.neste = p;
           current.neste = tmp;
+          size++;
+          return;
         } else {
           current = p;
+          if (p.neste == null) {
+            p.neste = tmp;
+            size++;
+            return;
+          }
           p = p.neste;
         }
       }
     }
-    size++;
+  }
+
+  public T fjern() {
+    return fjern(size-1);
+  }
+
+  public void sett(int pos, T x) {
+    throw new UnsupportedOperationException();
+  }
+  public void leggTil(int pos, T x) {
+    throw new UnsupportedOperationException();
   }
 }
