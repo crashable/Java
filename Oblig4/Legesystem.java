c004c780aebf54696a89e0a3c488e5604ee85645
Nghi Ng <Nghi_96@hotmail.com>
2019-03-21 20:17:40 +0100

    Debugged everything fine, Lege and Pasient is not going through the tests, does not receive values.

--- Oblig4/Legesystem.java
+++ Oblig4/Legesystem.java
@@ -15,6 +15,10 @@ public class Legesystem{
             System.out.println("Fant ikke filen, starter opp som et tomt Legesystem");
             return;
         }
+        Lenkeliste<Pasient> pasienter = new Lenkeliste<Pasient>();
+        Lenkeliste<Legemiddel> legemidler = new Lenkeliste<Legemiddel>();
+        Lenkeliste<Lege> leger = new Lenkeliste<Lege>();
+        Lenkeliste<Resept> resepter = new Lenkeliste<Resept>();
 
         String innlest = scanner.nextLine();
 
@@ -25,7 +29,6 @@ public class Legesystem{
 
             // Legger til alle pasientene i filen
             if(info[1].compareTo("Pasienter") == 0){
-                Lenkeliste<Pasient> pasienter = new Lenkeliste<Pasient>();
                 while(scanner.hasNextLine()) {
                     innlest = scanner.nextLine();
                     String[] inf = innlest.split(" ");
@@ -37,7 +40,6 @@ public class Legesystem{
             }
             //Legger inn Legemidlene
             else if(info[1].compareTo("Legemidler") == 0){
-                Lenkeliste<Legemiddel> legemidler = new Lenkeliste<Legemiddel>();
                 while(scanner.hasNextLine()){
                     innlest = scanner.nextLine();
                     if(innlest.charAt(0) == '#'){
@@ -64,7 +66,6 @@ public class Legesystem{
             }
             //Legger inn leger
             else if(info[1].compareTo("Leger") == 0){
-                Lenkeliste<Lege> leger = new Lenkeliste<Lege>();
                 while(scanner.hasNextLine()){
                     innlest = scanner.nextLine();
                     if(innlest.charAt(0) == '#'){
@@ -82,14 +83,39 @@ public class Legesystem{
             }
             //Legger inn Resepter
             else if(info[1].compareTo("Resepter") == 0){
-                Lenkeliste<Resept> resepter = new Lenkeliste<Resept>();
                 while(scanner.hasNextLine()){
+                    int index = Integer.parseInt(info[0]);
                     innlest = scanner.nextLine();
                     info = innlest.split(", ");
-                    if(info[0].compareTo("0")) {
-                      //I'm a bit stuck on how to proceed, for-loop? if's? Combo? Bedtime.
-                        resepter.leggTil(new Resept(legemidler[0]))
+                    //Put all of this in a for-loop for each legemiddel?
+                    //if(info[0].compareTo("0"))
+                    //Try to use the get function to check if it's correct, then add in.
+                    //Lege lege = new Lege("ere");
+                    //Pasient pasient  = new Pasient("ere", "123");
+                    Lege lege;
+                    Pasient pasient;
+                    for (int i=0; i<leger.stoerrelse()+1; i++) {
+                        Lege legeTest = leger.hent(i);
+                        if (info[1].compareTo(legeTest.hentNavn()) == 0) {
+                            lege = leger.hent(i);
+                        }
                     }
+                    for (int i=0; i<pasienter.stoerrelse()+1; i++) {
+                        Pasient pasientTest = pasienter.hent(i);
+                        String id = Integer.toString(pasientTest.getID());
+                        //int pasID = Integer.parseInt(info[2]);
+                        if (info[2].compareTo(id) == 0) {
+                            pasient = pasienter.hent(i);
+                        }
+                    }
+                    int reit = Integer.parseInt(info[3]);
+                    try {
+                        resepter.leggTil(lege.skrivResept(legemidler.hent(index), pasient, reit));
+                    } catch(UlovligUtskrift e) {
+                        System.out.println(e.getMessage());
+                    }
+
+
                     //
                     // Her må du finne legen, legemiddelet, og pasienten som ligger
                     // i lenkelistene utifra informasjonen.
